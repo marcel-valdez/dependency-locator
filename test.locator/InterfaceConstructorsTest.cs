2609b9b3e6700105f25e7cacad7d4ddf0a63faae
Marcel Valdez <marcel.valdez@live.com>
2012-04-10 15:09:08 -0500

    DLOC-1 #resolve Fixed method names * InterfaceConstructorsTest.cs: Se arreglaron los nombres de los métodos de pruebas.

--- test.locator/InterfaceConstructorsTest.cs
+++ test.locator/InterfaceConstructorsTest.cs
@@ -88,7 +88,7 @@
         ///A test for IsType
         ///</summary>
         [TestMethod()]
-        public void CanVerifyItsType()
+        public void CanVerifyATypeIsEqualOrUnequal()
         {
             // Arrange
             Type interfaceType = typeof(IStubDependency);
@@ -128,7 +128,7 @@
         }
 
         [TestMethod()]
-        public void IsTypeTest1()
+        public void CanVerifyASubTypeIsOfABaseType()
         {
             IsTypeTest1Helper<IStubDependency, IStubDependency>(true);
             IsTypeTest1Helper<IStubDependency, IDisposable>(false);
@@ -154,10 +154,10 @@
         }
 
         /// <summary>
-        ///A test for TryGetConstructor
+        /// Tests if it can get a parameterless consructor (objec[] {}), a constructor with params, and not get a constructor that doesnt match the parameters
         ///</summary>
         [TestMethod()]
-        public void TryGetConstructorTest()
+        public void CanGetOrNotAnExistingConstructorWithEmptyParamsExistingParamsAndNonExistingParams()
         {
             // Arrange
             Type interfaceType = typeof(IStubDependency);
@@ -201,58 +201,35 @@
         }
 
         /// <summary>
-        ///A test for TryGetConstructor
+        /// Tests that it can get a parameterless constructor using Type.EmptyTypes as the argument
         ///</summary>
         [TestMethod()]
-        public void TryGetConstructorTest1()
+        public void CanGetAConstructorUsingTypeEmptyTypesAsParams()
         {
             // Arrange
             Type interfaceType = typeof(IStubDependency);
             InterfaceConstructorsContainer target;
             ConstructorInvoker ctor1 = null;
-            ConstructorInvoker ctor2 = null;
-            ConstructorInvoker ctor3 = null;
             Type[] emptyParams = Type.EmptyTypes;
-            Type[] existingParams = Type.GetTypeArray(new object[] { "1", 2, "3" });
-            Type[] nonExistingParams = Type.GetTypeArray(new object[] { "1", 2, "3", 4 });
             bool existing1;
-            bool existing2;
-            bool nonexisting;
 
             // Act
             target = new InterfaceConstructorsContainer(interfaceType)
                     .SetConcrete<ConcreteStubDependency>();
             existing1 = target.TryGetConstructor(out ctor1, emptyParams);
-            existing2 = target.TryGetConstructor(out ctor2, existingParams);
-            nonexisting = target.TryGetConstructor(out ctor3, nonExistingParams);
 
             // Assert
             Verify.That(existing1).IsTrue().Now();
             Verify.That(ctor1).IsNotNull().Now();
-
-            Verify.That(existing2).IsTrue().Now();
-            Verify.That(ctor2).IsNotNull().Now();
-
-            Verify.That(nonexisting).IsFalse().Now();
-            Verify.That(ctor3).IsNull().Now();
-
-            Verify.That(ctor1)
-                  .IsNotEqualTo(ctor2)
-                  .And()
-                  .IsNotEqualTo(ctor3)
-                  .Now();
-
-            Verify.That(ctor2)
-                  .IsNotEqualTo(ctor3)
-                  .Now();
         }
 
 
         /// <summary>
-        ///A test for TryGetConstructor
+        /// Tests wether it can get the constructor using parameters that derive from the actual parameters of the constructor
+		/// and the actual parameter types of the constructor
         ///</summary>
         [TestMethod()]
-        public void TryGetFromInheritedTypes()
+        public void CanGetAConstructorUsingInheritingTypeOrExactTypesAsParameters()
         {
             // Arrange
             Type interfaceType = typeof(IConstructorableStub);
